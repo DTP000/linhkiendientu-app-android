d2c2feddaaea20b0e59d2b6193096119dc359673
youkino0101 <77605935+youkino0101@users.noreply.github.com>
2022-09-30 19:20:22 +0700

    trang home 1/2

--- app/src/main/java/tk/dtp000/linhkiendientu/MainActivity.java
+++ app/src/main/java/tk/dtp000/linhkiendientu/MainActivity.java
@@ -3,12 +3,52 @@ package tk.dtp000.linhkiendientu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getViewId();
+        Slider();
+
+    }
+
+    private void Slider() {
+        List<String> slider = new ArrayList<>();
+        slider.add(getString(R.string.slide_1));
+        slider.add(getString(R.string.slide_2));
+        slider.add(getString(R.string.slide_3));
+        for (int i = 0; i< slider.size();i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext())
+                    .load(slider.get(i))
+                    .into(imageView);
+
+            //fix imageView vào ViewFlipper
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_step_1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slider_step_1);
+        Animation animation_slide_step_2 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slider_step_2);
+
+        viewFlipper.setInAnimation(animation_slide_step_1);
+        viewFlipper.setOutAnimation(animation_slide_step_2);
+    }
+
+    private void getViewId() {
+        viewFlipper = findViewById(R.id.viewFlipper);
     }
 }
